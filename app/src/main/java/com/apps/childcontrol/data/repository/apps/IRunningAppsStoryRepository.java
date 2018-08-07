7171a3746143b55ec2c66d260c61d0a2ecd63883
Icoper <gotoyougoal@gmail.com>
2018-08-07 19:55:27 +0300

    updated IRunningAppsStoryRepository

--- app/src/main/java/com/apps/childcontrol/data/repository/apps/IRunningAppsStoryRepository.java
+++ app/src/main/java/com/apps/childcontrol/data/repository/apps/IRunningAppsStoryRepository.java
@@ -1,13 +1,13 @@
 package com.apps.childcontrol.data.repository.apps;
 
-import com.apps.childcontrol.data.model.IApplicationInfo;
+import com.apps.childcontrol.data.model.appInfo.IApplicationInfo;
 
 import java.util.List;
 
 public interface IRunningAppsStoryRepository {
     void add(IApplicationInfo applicationInfo);
 
-    void update(IApplicationInfo applicationInfo);
+    void repeatLastAppInfo(IApplicationInfo applicationInfo);
 
     List<IApplicationInfo> getAll();
 
--- app/src/main/java/com/apps/childcontrol/data/repository/apps/InLocalDBRunningAppsStoryRepository.java
+++ app/src/main/java/com/apps/childcontrol/data/repository/apps/InLocalDBRunningAppsStoryRepository.java
@@ -1,27 +1,79 @@
 package com.apps.childcontrol.data.repository.apps;
 
-import com.apps.childcontrol.data.model.IApplicationInfo;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
+import com.apps.childcontrol.data.model.appInfo.ApplicationInfo;
+import com.apps.childcontrol.data.model.appInfo.IApplicationInfo;
+import com.apps.childcontrol.data.repository.apps.sqldb.AppsBaseDataHelper;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class InLocalDBRunningAppsStoryRepository implements IRunningAppsStoryRepository {
-    @Override
-    public void add(IApplicationInfo applicationInfo) {
 
+    private final PackageManager packageManager;
+    private final AppsBaseDataHelper dataHelper;
+
+    private SQLiteDatabase database;
+
+    public InLocalDBRunningAppsStoryRepository(
+            PackageManager packageManager,
+            AppsBaseDataHelper dataHelper) {
+
+        this.packageManager = packageManager;
+        this.dataHelper = dataHelper;
     }
 
     @Override
-    public void update(IApplicationInfo applicationInfo) {
+    public void add(IApplicationInfo applicationInfo) {
+        // TODO: 07.08.2018  
+    }
 
+    @Override
+    public void repeatLastAppInfo(IApplicationInfo applicationInfo) {
+        // TODO: 07.08.2018  
     }
 
     @Override
     public List<IApplicationInfo> getAll() {
-        return null;
+        List<IApplicationInfo> infoArrayList = new ArrayList<>();
+
+        if (database == null || !database.isOpen()) {
+            database = dataHelper.getWritableDatabase();
+        }
+        String query = "SELECT  * FROM " +
+                AppsBaseDataHelper.Application.TABLE_NAME;
+
+        Cursor cursor = database.rawQuery(query, null);
+
+
+        try {
+            cursor.moveToFirst();
+
+            while (!cursor.isAfterLast()) {
+                IApplicationInfo app =
+                        new ApplicationInfo(
+                                cursor.getString(cursor.getColumnIndexOrThrow(AppsBaseDataHelper.Application.APP_NAME)),
+                                cursor.getString(cursor.getColumnIndexOrThrow(AppsBaseDataHelper.Application.APP_PACKAGE)),
+                                cursor.getLong(cursor.getColumnIndexOrThrow(AppsBaseDataHelper.Application.APP_TIME_START)),
+                                cursor.getLong(cursor.getColumnIndexOrThrow(AppsBaseDataHelper.Application.APP_TIME_END)));
+
+                infoArrayList.add(app);
+                cursor.moveToNext();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        dataHelper.close();
+        cursor.close();
+
+        return infoArrayList;
     }
 
     @Override
     public void deleteAll() {
-
+        // TODO: 07.08.2018  
     }
 }
