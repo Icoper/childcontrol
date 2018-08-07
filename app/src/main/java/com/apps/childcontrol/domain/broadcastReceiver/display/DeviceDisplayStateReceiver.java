bec047f8bbfbd1d36ce96a0c82cc72cd6140bd2c
Icoper <gotoyougoal@gmail.com>
2018-08-07 19:53:14 +0300

    updated DeviceDisplayStateReceiver

---
+++ app/src/main/java/com/apps/childcontrol/domain/broadcastReceiver/display/DeviceDisplayStateReceiver.java
@@ -0,0 +1,12 @@
+package com.apps.childcontrol.domain.broadcastReceiver.display;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class DeviceDisplayStateReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
